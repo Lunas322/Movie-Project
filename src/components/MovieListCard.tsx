da2c3f00617db4056fc70c253c39a96e0dee0255
Lunas322 <fpvmxm333477@gmail.com>
2026-05-25 22:38:33 +0900

    feat: Recent 데이터 추가 함수 추가

--- src/components/MovieListCard.tsx
+++ src/components/MovieListCard.tsx
@@ -1,29 +1,39 @@
-import { useNavigate } from "react-router-dom"
-import type {MovieType } from "../types/movieTypes"
+import { useNavigate } from "react-router-dom";
+import type { MovieType } from "../types/movieTypes";
+import { useContext } from "react";
+import { RecentContext } from "../context/RecentContext";
 
+function MovieListCard({ poster_path, title, vote_average, id }: MovieType) {
+  const nav = useNavigate();
+  const context = useContext(RecentContext);
+  if (!context) return null;
+  const { setRecentMovie } = context;
+  function recentAdd() {
+    setRecentMovie((prev) => {
+      const filter = prev.filter((movie) => movie.id !== id);
+      return [{ id, poster_path, title, vote_average }, ...filter];
+    });
+    nav(`/detail/${id}`);
+  }
 
-function MovieListCard ({poster_path,title,vote_average,id}:MovieType) {
-    const nav = useNavigate()
-    return (
-        <>
-        <div className="bg-zinc-900 rounded-xl overflow-hidden hover:scale-105 transition duration-300 cursor-pointer"
-        onClick={()=>nav(`detail/${id}`)}>
-            <img
-              src={`https://image.tmdb.org/t/p/w300${poster_path}`}
-              className="w-full h-72"
-            />
+  return (
+    <>
+      <div
+        className="bg-zinc-900 rounded-xl overflow-hidden hover:scale-105 transition duration-300 cursor-pointer"
+        onClick={() => recentAdd()}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+          className="w-full h-72"
+        />
 
-            <div className="p-4">
-              <h3 className="text-white text-lg font-semibold truncate">
-                {title}
-              </h3>
+        <div className="p-4">
+          <h3 className="text-white text-lg font-semibold truncate">{title}</h3>
 
-              <p className="text-gray-400 text-sm mt-1">
-                ⭐ {vote_average}
-              </p>
-            </div>
-          </div>
-        </>
-    )
+          <p className="text-gray-400 text-sm mt-1">⭐ {vote_average}</p>
+        </div>
+      </div>
+    </>
+  );
 }
-export default MovieListCard
+export default MovieListCard;
