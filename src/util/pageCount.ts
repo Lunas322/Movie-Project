8ef0216916cf0e22f3edc7117c6cc9539cdd38df
Lunas322 <fpvmxm333477@gmail.com>
2026-05-26 02:48:40 +0900

    refactor: 불필요 util 제거

--- src/util/pageCount.ts
+++
@@ -1,8 +0,0 @@
-import type { Dispatch, SetStateAction } from "react";
-type pageCountProps = {
-  setPage: Dispatch<SetStateAction<number>>;
-};
-
-export function pageCount({ setPage }: pageCountProps) {
-  setPage((prev) => prev + 1);
-}
