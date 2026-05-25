de25900a11fb76b561e39117add7357158975f95
Lunas322 <fpvmxm333477@gmail.com>
2026-05-26 01:00:26 +0900

    feat: contextAPI Provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './index.css'
 import {RouterProvider } from 'react-router-dom'
 import { router } from './router/router.tsx'
 import { RecentProvider } from './context/RecentContext.tsx'
+import { WishListProvider } from './context/WishlistContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <RecentProvider>
+    <WishListProvider>
     <RouterProvider router={router}/>
+    </WishListProvider>
     </RecentProvider>
 )
