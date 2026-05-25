3432aa575d0f483f1a40c70de373951250f6a4a5
Lunas322 <fpvmxm333477@gmail.com>
2026-05-25 21:22:35 +0900

    refactor: api 호출 구조 및 UI 분기 처리 개선

--- src/components/MovieListContainer.tsx
+++ src/components/MovieListContainer.tsx
@@ -1,38 +1,39 @@
-import { useEffect, useState } from "react";
-import { getMovieData } from "../api/movie";
 import type { MovieType } from "../types/movieTypes";
 import MovieListCard from "./MovieListCard";
 import { pageCount } from "../util/pageCount";
 
+type Props = {
+  movieData: MovieType[];
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+};
 
-
-function MovieListContainer() {
-  const [movieData, setMovieData] = useState<MovieType[]>([]);
-  const [page,setPage] = useState(1)
-  
-
-  useEffect(() => {
-    async function movieList() {
-      try {
-        const data = await getMovieData(page);
-        setMovieData((prev)=> [...prev, ...data]);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    movieList();
-  }, [page]);
-
+function MovieListContainer({ movieData, setPage }: Props) {
   return (
-    <div className="w-full min-h-screen bg-black px-10 py-4 flex justify-center flex-col items-center gap-7">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+    <div className="w-full flex flex-col items-center gap-10">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 w-full justify-items-center">
+
         {movieData?.map((item) => (
-          <MovieListCard id={item.id} title={item.title} vote_average={item.vote_average} poster_path={item.poster_path}/>
+          <div key={item.id} className="w-full max-w-55">
+            <MovieListCard
+              id={item.id}
+              title={item.title}
+              vote_average={item.vote_average}
+              poster_path={item.poster_path}
+            />
+          </div>
         ))}
+
       </div>
-      <button onClick={()=>pageCount({setPage})} className="w-90 h-14 bg-white rounded-2xl border border-gray-300 text-gray-800 font-medium shadow-sm hover:bg-gray-300 transition">
-  더보기
-</button>
+
+      {movieData?.length > 10 && (
+        <button
+          onClick={() => pageCount({ setPage })}
+          className="w-90 h-14 bg-white rounded-2xl border border-gray-300 text-gray-800 font-medium shadow-sm hover:bg-gray-300 transition"
+        >
+          더보기
+        </button>
+      )}
+
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header"
 import MovieListContainer from "../components/MovieListContainer"
+import type { MovieType } from "../types/movieTypes";
+import { getMovieData } from "../api/movie";
 
 function Home () {
+
+      const [movieData, setMovieData] = useState<MovieType[]>([]);
+  const [page,setPage] = useState(1)
+  
+
+  useEffect(() => {
+    async function movieList() {
+      try {
+        const data = await getMovieData(page);
+        setMovieData((prev)=> [...prev, ...data]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    movieList();
+  }, [page]);
     return (
         <>
-        <div className="w-screen h-screen bg-white">
         <Header/>
-        <MovieListContainer/>
+        <div className="w-screen h-full bg-black py-10">
+        <MovieListContainer movieData={movieData} setPage={setPage}/>
         </div>
         </>
     )
